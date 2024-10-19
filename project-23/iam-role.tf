7ddc0af203e8f6f9466498b485c341a8b9a2807a
Marco Lutz <lutz_m@fev.io>
2024-10-19 09:23:07 +0200

    Updates

--- project-23/iam-role.tf
+++ project-23/iam-role.tf
@@ -29,21 +29,21 @@ resource "aws_iam_role" "external-admin" {
       }
     ]
   })
+}
 
-  inline_policy {
-    name = "external-admin-policy"
-
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action   = ["eks:DescribeCluster"]
-          Effect   = "Allow"
-          Resource = "*"
-        }
-      ]
-    })
-  }
+resource "aws_iam_role_policy" "external-admin-policy" {
+  name = "external-admin-policy"
+  role = aws_iam_role.external-admin.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["eks:DescribeCluster"]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role" "external-developer" {
@@ -62,19 +62,19 @@ resource "aws_iam_role" "external-developer" {
       }
     ]
   })
+}
 
-  inline_policy {
-    name = "external-developer-policy"
-
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action   = ["eks:DescribeCluster"]
-          Effect   = "Allow"
-          Resource = "*"
-        }
-      ]
-    })
-  }
+resource "aws_iam_role_policy" "external-developer-policy" {
+  name = "external-developer-policy"
+  role = aws_iam_role.external-developer.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["eks:DescribeCluster"]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
 }
